5ec05344403ae37a01e838c5f26348d4dc5ba27c
Ejko <liliya.stefanova@gmail.com>
2013-11-09 17:14:32 +0000

    This is an enumerated type which specifies the possible values for the aircraft aim game

---
+++ Result.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: liliya
+ * Date: 04/11/13
+ * Time: 20:47
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Result {
+    HIT, FAIL_LEFT,FAIL_RIGHT,FAIL_HIGH, FAIL_LOW,
+    FAIL_SHORT, FAIL_LONG,OUT_OF_RANGE
+}
---
+++ Target.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: liliya
+ * Date: 04/11/13
+ * Time: 20:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Target {
+}

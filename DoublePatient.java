796b009fa9d9af7b6254007599bcc487d4cc5ba2
Ejko <liliya.stefanova@gmail.com>
2013-11-14 22:28:31 +0000

    an example of a doubly linked list using the same code added a pointer for previousPatient and another getter method for the previousPatient pointer linked to DoubleHospitalManager.java, where the list is initialized and the launch method lives

---
+++ DoublePatient.java
@@ -0,0 +1,74 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: liliya
+ * Date: 14/11/13
+ * Time: 20:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class DoublePatient {
+     private String name;
+     private int age;
+     private String illness;
+     private DoublePatient nextPatient;
+     private DoublePatient previousPatient;
+
+     public DoublePatient(String name, int age, String illness){
+        this.name=name;
+        this.age=age;
+        this.illness=illness;
+        this.nextPatient=null;
+        this.previousPatient=null;
+     }
+     public void addPatient(DoublePatient newPatient){
+        if(this.nextPatient==null){
+            this.nextPatient=newPatient;
+            newPatient.previousPatient=this;
+        }
+        else{
+
+            this.nextPatient.addPatient(newPatient);
+         }
+     }
+
+    public boolean deletePatient(DoublePatient patientToDelete){
+        if(this.nextPatient==null){
+             return false;
+        }
+        else if(this.nextPatient.name.equals(patientToDelete.name)){
+            this.nextPatient.previousPatient=this;
+            this.nextPatient=nextPatient.nextPatient;
+
+        return true;
+        }
+
+        else{
+            return  this.nextPatient.deletePatient(patientToDelete);
+        }
+
+        }
+    public DoublePatient getNext(){
+            return nextPatient;
+    }
+
+    public DoublePatient getPrevious(){
+            return previousPatient;
+    }
+
+    public String getName(){
+            return name;
+    }
+
+    public int getAge(){
+            return age;
+    }
+    public String getIllness(){
+            return illness;
+    }
+
+    public void Print(){
+
+            System.out.println("Patient: "+ this.getName()+"; Age: "+ this.getAge()+"; Illness: "+ this.getIllness());
+
+           }
+
+}

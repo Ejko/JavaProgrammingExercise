f7d8f59380bcda413876922dd944a14bbbf2ebf0
Ejko <liliya.stefanova@gmail.com>
2014-01-12 23:56:13 +0000

    amended the patient node class to include some additional methods for searching a patient, setting previous and next patient reference

--- DoubleLinkedList/DoublePatient.java
+++ DoubleLinkedList/DoublePatient.java
@@ -7,7 +7,17 @@ package DoubleLinkedList;
  * Time: 20:33
  * To change this template use File | Settings | File Templates.
  */
+
+/**
+ * Implementation of a double linked list using patient records as nodes
+ * Add and remove methods in this version are in the node class(DoublePatient) as opposed to in the list class
+ * Note: Not a very easy to test implementation; have had to introduce some additional methods to access data
+ */
+
 public class DoublePatient {
+    /**
+     * Patient attributes, includes reference to next and previous patient
+     */
      private String name;
      private int age;
      private String illness;
@@ -21,6 +31,12 @@ public class DoublePatient {
         this.nextPatient=null;
         this.previousPatient=null;
      }
+
+    /**
+     * Adding a new patient to the list
+     * Special cases: adding to an empty list
+     * @param newPatient
+     */
      public void addPatient(DoublePatient newPatient){
         if(this.nextPatient==null){
             this.nextPatient=newPatient;
@@ -31,6 +47,13 @@ public class DoublePatient {
             this.nextPatient.addPatient(newPatient);
          }
      }
+
+    /**
+     * Deleting a patient from a list
+     * Special cases-not finding the required patient
+     * @param patientToDelete
+     * @return true is the patient is found and deleted; false if the patient is not found
+     */
 
     public boolean deletePatient(DoublePatient patientToDelete){
         if(this.nextPatient==null){
@@ -50,9 +73,29 @@ public class DoublePatient {
         }
 
      }
+
+    /**
+     * New method implemented to return the name of a patient in the list
+     * @param patient
+     * @return the name of
+     */
+
+    public String searchPatient(DoublePatient patient){
+        if(this.nextPatient==null){
+            return null;
+        }
+        else if(this.nextPatient.name.equals(patient.name)){
+            return nextPatient.name;
+        }
+        else{
+            return this.nextPatient.searchPatient(patient);
+        }
+    }
+
     public DoublePatient getNext(){
             return nextPatient;
     }
+
 
     public DoublePatient getPrevious(){
             return previousPatient;
@@ -69,14 +112,22 @@ public class DoublePatient {
             return illness;
     }
 
+    public void setPrevious(DoublePatient patient){
+       previousPatient=patient;
+    }
+
+    public void setNext(DoublePatient patient){
+        nextPatient=patient;
+    }
+
     @Override
     public String toString() {
-        return "DoubleLinkedList.DoublePatient(" + this.getName() + ")";
+        return this.getName();
     }
 
     public void Print(){
 
-            System.out.println("Patient: "+ this.getName()+"; Age: "+ this.getAge()+"; Illness: "+ this.getIllness());
+            System.out.println("SinglyLinkedList.Patient: "+ this.getName()+"; Age: "+ this.getAge()+"; Illness: "+ this.getIllness());
 
            }
 
